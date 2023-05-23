c66083887c70661313f66eb944edab73b26e6557
Moeeru <126564425+Moeeru@users.noreply.github.com>
2023-05-23 09:43:53 -0300

    bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -6,6 +6,21 @@ import "errors"
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
 func Destino(caminhos [][2]string) (string, error) {
-	// Seu código aqui
-	return "", errors.New("Not implemented yet")
+	if len(caminhos) == 0 {
+		return "", errors.New("not implemented yet")
+	}
+	contador := make(map[string]int)
+	for i:= 0; i < len(contador); i++{
+		for j:= 0; j < len(contador); j++{
+			if caminhos[0][i] == caminhos[0][j]{
+				contador[caminhos[0][i]] ++
+			}
+		}
+	}
+	for chave, valor := range contador {
+		if valor == 1 {
+			return chave, nil
+		}
+	}
+	return "",nil
 }
